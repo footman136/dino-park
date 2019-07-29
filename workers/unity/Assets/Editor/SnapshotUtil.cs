1721ccfcb8a99d42d74c1a3ea4f422956affaf10
footman136 <liugang2006@hotmail.com>
2019-07-29 12:27:26 +0800

    删除了没用的EntityCache目录和EneityPrefabs

--- workers/unity/Assets/Editor/SnapshotUtil.cs
+++ workers/unity/Assets/Editor/SnapshotUtil.cs
@@ -1,11 +1,7 @@
-using Assets.Gamelogic.Core;
 using UnityEngine;
 using Improbable;
-using DinoPark; // UnityClientConnector UnityGameLogicConnector
-// EntityTemplateFactory
+using Assets.Gamelogic.Core;
 using Snapshot = Improbable.Gdk.Core.Snapshot;
-using Improbable.Gdk.Core; // EntityTemplate
-using Improbable.Gdk.PlayerLifecycle; //PlayerCreator
 
 namespace Editor
 {
@@ -15,18 +11,8 @@ namespace Editor
 
         public static void AddPlayerSpawner(Snapshot snapshot)
         {
-            var serverAttribute = UnityGameLogicConnector.WorkerType;
-
-            var template = new EntityTemplate();
-            template.AddComponent(new Position.Snapshot(), serverAttribute);
-            template.AddComponent(new Metadata.Snapshot { EntityType = "PlayerCreator" }, serverAttribute);
-            template.AddComponent(new Persistence.Snapshot(), serverAttribute);
-            template.AddComponent(new PlayerCreator.Snapshot(), serverAttribute);
-
-            template.SetReadAccess(UnityClientConnector.WorkerType, UnityGameLogicConnector.WorkerType, MobileClientWorkerConnector.WorkerType);
-            template.SetComponentWriteAccess(EntityAcl.ComponentId, serverAttribute);
-
-            snapshot.AddEntity(template);
+            var entity = EntityTemplateFactory.CreatePlayerTemplate(snapshot);
+            snapshot.AddEntity(entity);
         }
 
         public static void AddTrees(Snapshot snapshot, Texture2D sampler, float sampleThreshold, int countAproximate, float edgeLength, float placementJitter)
--- workers/unity/Assets/GameLogic/Core/EntityTemplateFactory.cs
+++ workers/unity/Assets/GameLogic/Core/EntityTemplateFactory.cs
@@ -6,6 +6,8 @@ using DinoPark;
 using Improbable;
 using Improbable.Gdk.Core;
 using UnityEngine;
+using Improbable.Gdk.PlayerLifecycle;
+
 // Random
 // Coordinates
 // WorkerUtils
@@ -14,8 +16,8 @@ using UnityEngine;
 // Health
 // TransformComponent
 // Flammable
-
 // EntityTemplate
+// PlayerCreator
 
 namespace Assets.Gamelogic.Core
 {
@@ -23,34 +25,39 @@ namespace Assets.Gamelogic.Core
     {
 	    public static EntityTemplate CreateTreeTemplate(Vector3f position, uint initialRotation)
 	    {
-		    var entityTemplate = new EntityTemplate();
-		    entityTemplate.AddComponent(new Position.Snapshot(new Coordinates(position.X, position.Y, position.Z)), WorkerUtils.UnityGameLogic);
-		    entityTemplate.AddComponent(new Metadata.Snapshot(SimulationSettings.TreePrefabName), WorkerUtils.UnityGameLogic);
-		    entityTemplate.AddComponent(new Persistence.Snapshot(), WorkerUtils.UnityGameLogic);
-		    entityTemplate.AddComponent(new TransformComponent.Snapshot(initialRotation), WorkerUtils.UnityGameLogic);
-		    entityTemplate.AddComponent(new Harvestable.Snapshot(), WorkerUtils.UnityGameLogic);
-		    entityTemplate.AddComponent(new Health.Snapshot(SimulationSettings.TreeMaxHealth, SimulationSettings.TreeMaxHealth, true), WorkerUtils.UnityGameLogic);
-		    entityTemplate.AddComponent(new Flammable.Snapshot(false, true, FireEffectType.BIG), WorkerUtils.UnityGameLogic);
-		    var treeStateComponent = new Dinopark.Plants.TreeState.Snapshot((TreeType) Random.Range(0, 2), TreeFSMState.HEALTHY);
-		    entityTemplate.AddComponent(treeStateComponent, WorkerUtils.UnityGameLogic);
+		    var serverAttribute = UnityGameLogicConnector.WorkerType;
 		    
-		    entityTemplate.SetReadAccess(WorkerUtils.UnityGameLogic, WorkerUtils.UnityClient);
-		    entityTemplate.SetComponentWriteAccess(EntityAcl.ComponentId, WorkerUtils.UnityGameLogic);
+		    var entityTemplate = new EntityTemplate();
+		    entityTemplate.AddComponent(new Position.Snapshot(new Coordinates(position.X, position.Y, position.Z)), serverAttribute);
+		    entityTemplate.AddComponent(new Metadata.Snapshot(SimulationSettings.TreePrefabName), serverAttribute);
+		    entityTemplate.AddComponent(new Persistence.Snapshot(), serverAttribute);
+		    entityTemplate.AddComponent(new TransformComponent.Snapshot(initialRotation), serverAttribute);
+		    entityTemplate.AddComponent(new Harvestable.Snapshot(), serverAttribute);
+		    entityTemplate.AddComponent(new Health.Snapshot(SimulationSettings.TreeMaxHealth, SimulationSettings.TreeMaxHealth, true), serverAttribute);
+		    entityTemplate.AddComponent(new Flammable.Snapshot(false, true, FireEffectType.BIG), serverAttribute);
+		    var treeStateComponent = new Dinopark.Plants.TreeState.Snapshot((TreeType) Random.Range(0, 2), TreeFSMState.HEALTHY);
+		    entityTemplate.AddComponent(treeStateComponent, serverAttribute);
+		    
+		    entityTemplate.SetReadAccess(UnityClientConnector.WorkerType, UnityGameLogicConnector.WorkerType, MobileClientWorkerConnector.WorkerType);
+		    entityTemplate.SetComponentWriteAccess(EntityAcl.ComponentId, serverAttribute);
 		    
 		    return entityTemplate;
-//		    var template = EntityBuilder.Begin()
-//			    .AddPositionComponent(initialPosition.ToUnityVector(), CommonRequirementSets.PhysicsOnly)
-//			    .AddMetadataComponent(SimulationSettings.TreePrefabName)
-//			    .SetPersistence(true)
-//			    .SetReadAcl(CommonRequirementSets.PhysicsOrVisual)
-//			    .AddComponent(new UnityWorkerAuthorityCheck.Data(), CommonRequirementSets.PhysicsOnly)
-//			    .AddComponent(new TransformComponent.Data(initialRotation), CommonRequirementSets.PhysicsOnly)
-//			    .AddComponent(new Harvestable.Data(), CommonRequirementSets.PhysicsOnly)
-//			    .AddComponent(new Health.Data(SimulationSettings.TreeMaxHealth, SimulationSettings.TreeMaxHealth, true), CommonRequirementSets.PhysicsOnly)
-//			    .AddComponent(new Flammable.Data(false, true, FireEffectType.BIG), CommonRequirementSets.PhysicsOnly)
-//			    .AddComponent(new TreeState.Data((TreeType) Random.Range(0, 2), TreeFSMState.HEALTHY), CommonRequirementSets.PhysicsOnly)
-//			    .Build();
-//		    return template;
+	    }
+
+	    public static EntityTemplate CreatePlayerTemplate(Snapshot snapshot)
+	    {
+		    var serverAttribute = UnityGameLogicConnector.WorkerType;
+
+		    var template = new EntityTemplate();
+		    template.AddComponent(new Position.Snapshot(), serverAttribute);
+		    template.AddComponent(new Metadata.Snapshot { EntityType = "PlayerCreator" }, serverAttribute);
+		    template.AddComponent(new Persistence.Snapshot(), serverAttribute);
+		    template.AddComponent(new PlayerCreator.Snapshot(), serverAttribute);
+
+		    template.SetReadAccess(UnityClientConnector.WorkerType, UnityGameLogicConnector.WorkerType, MobileClientWorkerConnector.WorkerType);
+		    template.SetComponentWriteAccess(EntityAcl.ComponentId, serverAttribute);
+
+		    return template;
 	    }
 	}
 }
